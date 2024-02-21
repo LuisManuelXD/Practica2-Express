c0007d04905732366c96a955116f3fd22ec59e73
Luis Manuel <riostorresGamer@gmail.com>
2024-02-20 19:56:29 -0600

    Agregando nueva ruta task index

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import express from 'express'
 import CategoryRouter from './category.route'
+import TaskRouter from './task.route'
 
 const routerApi = (app) => {
   const router = express.Router()
   app.use('/api/v1', router)
   router.use('/categories', CategoryRouter)
+  router.use('/tasks', TaskRouter)
 }
 
 export default routerApi
